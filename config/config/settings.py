5d664254eed1d8b120f684199373b70680eab658
nency22 <nensipan123@gmail.com>
2026-09-01 11:26:34 +0530

    Add Render domain to allowed hosts

--- config/config/settings.py
+++ config/config/settings.py
@@ -30,7 +30,11 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [ 
+    "restaurant-3-uwi4.onrender.com",
+    "127.0.0.1",
+    "localhost",
+]
 
 
 # Application definition
