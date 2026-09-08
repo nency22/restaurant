aa93dc2b2af48029362bc7b153328cf824ab435a
nency22 <nensipan123@gmail.com>
2026-09-08 13:23:33 +0530

    update permissions.py

--- config/accounts/permissions.py
+++ config/accounts/permissions.py
@@ -2,3 +2,17 @@ from rest_framework.permissions import BasePermission
 class IsAdmin(BasePermission):
     def has_permission(self, request, view):
         return (request.user.is_authenticated and request.user.role == "ADMIN")
+
+class IsStaff(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and request.user.role == "STAFF"
+        )
+
+class IsCustomer(BasePermission):
+    def has_permission(self, request, view):
+        return (
+            request.user.is_authenticated
+            and request.user.role == "CUSTOMER"
+        )        
