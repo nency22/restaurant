d6dc087859eb30ab9c3a73de99373bd03c401848
nency22 <nensipan123@gmail.com>
2026-09-08 13:51:07 +0530

    admin.py

--- config/menu/admin.py
+++ config/menu/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Category, MenuItem,MenuItemCustomization
+admin.site.register(Category)
+admin.site.register(MenuItem)
+admin.site.register(MenuItemCustomization)
 
 # Register your models here.
