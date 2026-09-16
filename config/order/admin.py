1491d36e49fed26708990a15fc1a0ee9a5121135
nency22 <nensipan123@gmail.com>
2026-09-16 15:37:07 +0530

    admin update

--- config/order/admin.py
+++ config/order/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Order, OrderItem,OrderItemCustomization
+admin.site.register(Order)
+admin.site.register(OrderItem)
+admin.site.register(OrderItemCustomization)
+
+
 
 # Register your models here.
