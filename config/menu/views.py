55d4a3176688e2e6121d4c69f309ca332a15213b
nency22 <nensipan123@gmail.com>
2026-09-17 11:40:18 +0530

    menu views update

--- config/menu/views.py
+++ config/menu/views.py
@@ -19,25 +19,43 @@ from .serializers import (
 )
 
 class CategoryListCreateView(APIView):
+
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+
+        return [IsAdmin()]
+
     def get(self,request):
         categories=Category.objects.all()
         serializer=CategorySerializer(categories,many=True)
         return Response(serializer.data)
+
     @extend_schema(
         request=CategorySerializer,
         responses=CategorySerializer
     )
     def post(self,request):
         serializer=CategorySerializer(data=request.data)
+
         if serializer.is_valid():
             serializer.save()
 
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+
         return Response(
-         serializer.errors,
-         status=status.HTTP_400_BAD_REQUEST
-    )
+            serializer.errors,
+            status=status.HTTP_400_BAD_REQUEST
+        )
 class CategoryDetailView(APIView):
+    def get_permissions(self):
+        if self.request.method == 'GET':
+            return [AllowAny()]
+
+        return [IsAdmin()]
     def get_object(self,pk):
         try:
             return Category.objects.get(pk=pk)  
